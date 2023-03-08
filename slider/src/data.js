de3823c5e959ec48196c24f1c56d68b8ac45766a
molade43 <molade43@gmail.com>
2023-03-08 13:27:47 +0100

    created a react slider one page app, where you can slide images

---
+++ slider/src/data.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/Black-Suit---1_9f608683-2129-4321-8cd2-47efa69981b3_1000x.jpg?v=1574775511",
+    title: "SUMMER SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET YOURS AT 30% OFF OLD STOCKS",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/BlueChalkStripe_1_dac9cbfb-2ed0-4087-a2d4-23b782ce3767_600x.jpg?v=1675786293",
+    title: "DRY SEASON SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET YOURS AT 30% OFF OLD STOCKS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img:
+      "https://cdn.shopify.com/s/files/1/0692/4255/products/HeatheredNavySuit11_400x.jpg?v=1663620363",
+    title: "HARMATTAN SEASON SALES",
+    description: "DON'T COMPROMISE ON STYLE! GET YOURS AT 30% OFF OLD STOCKS",
+    bg: "fbf0f4",
+  },
+];
